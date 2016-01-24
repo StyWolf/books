6e65e336af7937cc828a015f8f5dde2536787248
SenLief <senlief@163.com>
2016-01-24 11:26:04 -0500

    add headers

--- books_requests.py
+++ books_requests.py
@@ -27,7 +27,14 @@ def login():
 		'password' : 'zh123456',
 		'jumpurl'   : url
 	}
-	resp = requests.post(login_url,params=data)
+	headers = {
+                'User-Agent' : 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0' ,
+                'Connection' : 'keep-alive',
+                'Accept'         : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                'Accept-Encoding' : 'gzip, deflate, sdch',
+                'Accept-Language' : 'zh-CN,zh;q=0.8'
+        }
+	resp = requests.post(login_url,params=data,headers=headers)
 	resp.encoding = 'gbk'
 #Cookies
 	cookies = resp.cookies
@@ -97,7 +104,7 @@ def content():
 
 def rg(url):
 	headers = {
-		'User-Agent' : 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36',
+		'User-Agent' : 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0' ,
 		'Connection' : 'keep-alive',
 		'Accept'	 : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
 		'Accept-Encoding' : 'gzip, deflate, sdch',
