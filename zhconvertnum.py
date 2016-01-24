15148fa033ea128b81c7065cb6b3964441bf8f2f
SenLief <senlief@163.com>
2016-01-25 01:55:56 +0800

    zhtonum

--- zhconvertnum.py
+++ zhconvertnum.py
@@ -1,16 +1,18 @@
 ﻿#!/ usr/bin/env python 
 #-*-coding=utf-8-*-
 
-import sys,re
+import sys,re,textwrap
 reload(sys)
 sys.setdefaultencoding( "utf-8" )
 
 dict ={u'零':0,u'一':1,u'二':2,u'两':2,u'三':3,u'四':4,u'五':5,u'六':6,u'七':7,u'八':8,u'九':9,u'十':10,u'百':100,u'千':1000,u'万':10000,u'亿':100000000}
-def zhconvertnum(string):
+
+#万以下基础转换
+def zhconvertnum(chr):
 	tmp = []
 	count = 0
-	while count < len(string):
-		num = string[count:count+1]
+	while count < len(chr):
+		num = chr[count:count+1]
 		tmp.append(dict[num])
 		count = count + 1
 
@@ -40,6 +42,56 @@ def zhconvertnum(string):
 		y = tmp[0]
 
 	return y
+
+
+#亿以下转换
+def zhtonumwan(wan):
+	if wan.index(u'万') >= 2 :
+		index = wan.index(u'万')
+		wandown = wan[index+1:]
+		wanup = wan[:index]
+		num =  zhconvertnum(wanup) * 10000 + zhconvertnum(wandown)
+		return num
+	else:
+		num  = zhconvertnum(wan)
+		return num
+
+#千亿以下转换
+def zhtonum(string):
+	if string.index(u'亿') >= 1 :
+		index = string.index(u'亿')
+		yidown = string[index+1:]
+		yiup = string[:index]
+		wan = zhtonumwan(yidown)
+		num =  zhconvertnum(yiup) * 100000000 + wan
+		return ",".join(textwrap.wrap(str(num),3))
+	else:
+		num  = zhtonumwan(yi)
+		return ",".join(textwrap.wrap(str(num),3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+a = u'两亿零五千六百万两千零三百六十二万零三百'
+b = u'五十二万三千八百'
+c = u'三百四十八万四千'
+print zhtonum(a)
+
+#print a.index(u'万')
+'''
 
 '''
 #findall
